3cc675a8aabc67b3a07f7d987553649ef18a58d4
eguchi <eguchi@asial.co.jp>
2019-03-18 14:54:40 +0900

    TypeScript導入

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <HelloWorld msg="1" msg2="2"/>
   </div>
 </template>
 
