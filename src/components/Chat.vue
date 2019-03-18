00d6065f7db23ddb3e020e4700ba6e4a864abe04
eguchi <eguchi@asial.co.jp>
2019-03-18 15:58:21 +0900

    基礎組作成

---
+++ src/components/Chat.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="chat">
+    <p
+      v-for="(message, index) in messages"
+      v-bind:key="index">
+        {{ message }}
+    </p>
+    <section>
+      <div>
+        <input
+          type="text"
+          v-model="chat"
+          placeholder="input message here."/>
+        <button
+          :disabled="chat.length <= 0"
+          @click="handleClickChatSend(chat)">
+          決定
+        </button>
+      </div>
+    </section>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+
+@Component
+export default class Chat extends Vue {
+  // 「!はNon-null assertion operator。つまり、NotNullな値ってことの宣言
+  @Prop({ default: () => []})
+  public messages!: string[];
+  protected chat: string = '';
+
+  /**
+   * チャット送信文字決定
+   */
+  protected handleClickChatSend(chat: string): void {
+    if (!chat) {
+      return;
+    }
+    this.messages.push(chat);
+    this.$emit('emitClickChatSend', chat);
+    this.chat = '';
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,26 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="1" msg2="2"/>
+    <img
+      alt="Vue logo"
+      src="../assets/logo.png">
+    <Chat @emitClickChatSend="sendChat" />
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue';
+import Chat from '@/components/Chat.vue';
 
 @Component({
   components: {
     HelloWorld,
+    Chat,
   },
 })
-export default class Home extends Vue {}
+export default class Home extends Vue {
+  protected sendChat(chat: string): void {
+    alert(chat);
+  }
+}
 </script>
